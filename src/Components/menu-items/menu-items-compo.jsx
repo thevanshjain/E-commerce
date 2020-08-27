5956958e37689bab8b0fa1d5b74dd0d06c796f9a
vanshjain2810 <vanshvasu2810@gmail.com>
2020-08-27 23:34:32 +0530

    Finished styling of homepage

--- src/Components/menu-items/menu-items-compo.jsx
+++ src/Components/menu-items/menu-items-compo.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import './menu-items-styles.scss'
 
 const MenuItems = ({title,imageUrl,size}) => (
-    <div style={{
-        backgroundImage: `url(${imageUrl})`
-    }} 
-    className = { `${size} menu-item`}>
-                <div className='content'>
-                    <h1 className='title'>{title}</h1>
-                    <span className='subtitle'>Shop Now</span>
-                </div>
-            </div>
+    <div className = { `${size} menu-item`}>
+        <div 
+            className='background-image'  
+            style={{
+                backgroundImage: `url(${imageUrl})`
+                }} 
+        />
+        <div className='content'>
+            <h1 className='title'>{title.toUpperCase()}</h1>
+            <span className='subtitle'>Shop Now</span>
+        </div>
+    </div>
 );
 
 export default MenuItems;
