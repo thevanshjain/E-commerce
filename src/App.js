25da8c02f656a88fc7cc9baf751df88f81d4b56c
vanshjain2810 <vanshvasu2810@gmail.com>
2020-08-28 23:53:17 +0530

    React-router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import {Route} from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage-compo';
 import './App.css';
+
+const HatPage = () => (
+  <div>
+    <h1> HATS PAGE </h1>
+  </div>
+)
 
 function App() {
   return (
