3ec64cf11319214d62de26194517f08c2c11b50c
vanshjain2810 <vanshvasu2810@gmail.com>
2020-08-29 22:26:59 +0530

    Adding Header Component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Route, Switch} from 'react-router-dom';
 import ShopPage from './pages/Shop/Shop.component';
 import HomePage from './pages/homepage/homepage-compo';
+import Header from './Components/Header/Header-Component';
 import './App.css';
 
 
@@ -9,6 +10,7 @@ import './App.css';
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop/hats' component={ShopPage} />
--- src/Components/Collection-item/Collection-item-component.jsx
+++ src/Components/Collection-item/Collection-item-component.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 
 import './Collection-item-styles.scss';
 
-const CollectionItems =({id, name, price, imageUrl}) =>(
-<div className='collection-item'>
-    <div className='image'   
-        style={{
-            backgroundImage: `url(${imageUrl})`
-    }} />
-    <div className='collection-footer'>
-        <span className='name'>{name}</span>
-        <span className='price'>{price}</span>
+const CollectionItems = ({ id, name, price, imageUrl }) => (
+    <div className='collection-item'>
+        <div className='image'
+            style={{
+                backgroundImage: `url(${imageUrl})`
+            }} />
+        <div className='collection-footer'>
+            <span className='name'>{name}</span>
+            <span className='price'>{price}</span>
+        </div>
     </div>
-</div>
 
 );
 
