447df6f89adf499a849bec461136e226489606c5
vanshjain2810 <vanshvasu2810@gmail.com>
2020-09-13 17:56:38 +0530

    final production

--- client/src/Components/stripe-button/stripe-button-component.jsx
+++ client/src/Components/stripe-button/stripe-button-component.jsx
@@ -1,14 +1,31 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
+
 
 const StripeCheckoutButton =  ({price}) => {
     const priceForStripe = price * 100;
     const publishableKey = 'pk_test_51HO4QMDdMK2sVU2NoMEz0PJM6CbQTlgP1zfj47vxT0RGQhuKsG2YmtTXSo6Sp0L0zCKOBv5gakQpHkeTt4X7I33M00pGDEdNgb'
 
-const onToken = token => {
-    console.log(token);
-    alert("Payment Successful");
-}
+    const onToken = token => {
+        axios({
+          url: 'payment',
+          method: 'post',
+          data: {
+            amount: priceForStripe,
+            token: token
+          }
+        })
+          .then(response => {
+            alert('succesful payment');
+          })
+          .catch(error => {
+            console.log('Payment Error: ', error);
+            alert(
+              'There was an issue with your payment! Please make sure you use the provided credit card.'
+            );
+          });
+      };
     return(
         <StripeCheckout
          label='Pay Now'
